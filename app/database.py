a2cdc864a9e84c54f32237780d8d05d4fc24c94e
Bauws <lukas.zimmermann147@outlook.com>
2026-09-17 22:43:03 +0200

    refactor: load database url from settings

--- app/database.py
+++ app/database.py
@@ -1,14 +1,9 @@
-import os
-
-from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
-load_dotenv()
+from app.config import settings
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-engine = create_engine(DATABASE_URL)
+engine = create_engine(settings.database_url)
 SessionLocal = sessionmaker(bind=engine)
 
 class Base(DeclarativeBase):
