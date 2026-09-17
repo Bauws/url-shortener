768daa6b083d2f7a9ed50c3a6f7c1dbca3370916
Bauws <lukas.zimmermann147@outlook.com>
2026-09-17 03:32:35 +0200

    refactor: remove connection test code from database.py

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 import os
 
 from dotenv import load_dotenv
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
 load_dotenv()
@@ -9,13 +9,7 @@ load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
-
 SessionLocal = sessionmaker(bind=engine)
-
-with engine.connect() as e:
-    stmt = text("SELECT 1")
-    response = e.execute(stmt)
-    print(response.scalar())
 
 class Base(DeclarativeBase):
     pass
