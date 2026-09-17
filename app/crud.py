099846c54d6345709f79393fbaf7c280814630d7
Bauws <lukas.zimmermann147@outlook.com>
2026-09-18 01:45:03 +0200

    feat: add unique short code generation

--- app/crud.py
+++ app/crud.py
@@ -3,6 +3,7 @@ from sqlalchemy import select
 
 from app.models import Link
 from app.schemas import LinkCreate
+from app.utils import generate_short_code
 
 
 def create_link(db: Session, link_data: LinkCreate, short_code: str) -> Link:
@@ -14,3 +15,9 @@ def create_link(db: Session, link_data: LinkCreate, short_code: str) -> Link:
 
 def get_link_by_short_code(db: Session, short_code: str) -> Link | None:
     return db.scalar(select(Link).where(Link.short_code == short_code))
+
+def generate_unique_short_code(db: Session) -> str:
+    while True:
+        short_code = generate_short_code(7)
+        if get_link_by_short_code(db, short_code) is None:
+            return short_code
